2eac5739a63d1c694f1a2d3a1bfc908e44877ab6
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-04-19 13:58:40 +0500

    Issue 11

--- BASEMENT.py
+++ BASEMENT.py
@@ -6,19 +6,20 @@ ________________________________________________________________________________
 
 1. Extract Unique GET Params.
 2. If 2 forms have same 2 GET Params, then 4 GET URLs will be generated: DONE: later unique GET URLS are extracted
-3. Payload = (uvw"xyz'yxz</zxy. DONE
-4. Check encdoing separarately for each value of a context.     DONE
-    Issue: one value has encoding and others may not have..!!
-5. Base Url for all websites. Resolved THE ISSUE of ifu-institut.at and drudgereportarchives.com
-6. Now We Store the SUCCESSFUL ATTACK_URL, Context_name and Detection in the EXCEL FIle.. 
+3. Payload Updated = (uvw"xyz'yxz</zxy. DONE
+4. Encoding Check for each value of a context. 
+    a. Resolved Issue: one value has encoding and others may not have..!!
+5. Base Url for all websites. Resolved THE ISSUE of 'http://ifu-institut.at' and 'http://drudgereportarchives.com'
+6. Now We Store the 'SUCCESSFUL_ATTACK_URL', 'Context_name and 'Detection' in the EXCEL FIle.. 
+7. Created separate folder at Run-time for each WEBSITE, so all of its webpages data goes in that folder. 
+8. Unique GET URLs:
+    a. If [2] forms have same [2] GET Params, then [4] GET URLs will be generated and then [2] unique GET URLS are extracted
+    b. BUT if they have DIFFERENT FORM ACTIONS, then there will be 4 UNIQUE GET URLS.
 
 _____________________________________________________________________________________________________________________
 ==>>    UPDATES REQUIRED 
 
-1. How to Create a Folder through program
 2. Try the Vertical format of the EXCEL FILE.
-2. If 2 forms have same 2 GET Params, then 4 GET URLs will be generated: DONE: later unique GET URLS are extracted
-3. What if they have DIFFERENT FORM ACTIONS, then there should be 4 GET URLS.
 
 _____________________________________________________________________________________________________________________
 ==>> RESOLVED ISSUES:
@@ -56,13 +57,13 @@ Confirmed by Ashar as "Edge Cases" for title , span and style Tags
 4. payload </title><img src=x onerror="alert(1)"> is SUCCESSFUL
 ------------------------------------------------------------------------------------------
 
-ISSUE NO. 3: Attack Mehtodology for URL CONTEXT 
+ISSUE NO. 3: Attack Methodology for URL CONTEXT 
 
 1. Does it need any kind of Encoding Analysis..?
 2. Or for every webpage we try all of the attacks form Your Chart
 3. I'm Not very good with URL-Obfuscation, So, for now I have your payloads from js-fiddle
-    a. http://jsfiddle.net/Qv6F4/1/
-    b. http://jsfiddle.net/Qv6F4/
+    a. http://jsfiddle.net/Qv6F4/1/ : <a href="ja&#9;vasc&#10;ript&#58;alert&#40;1&#41;">click</a>
+    b. http://jsfiddle.net/Qv6F4/   : <a href="ja&#x00009;vasc&#x0000A;ript&#x0003A;alert&#x00028;1&#x00029;">click</a>
 
 ISSUE NO. 4:
 1. On https://www.crane.com
@@ -82,11 +83,10 @@ ISSUE NO. 5:    [Will Update it in the NEXT Versions]
     https://www.moosejaw.com/block.html?url=L2NvbnRlbnQvYW5uLWFyYm9yLXNob3A/&uuid=e19e1fc0-8173-11ea-9ed7-bd65ae1f2e66&vid=
 5. Block ID: e19e1fc0-8173-11ea-9ed7-bd65ae1f2e66
 
-
 ISSUE 6:    [Will Update it in the NEXT Versions]
 1. https://www.rei.com/
-2. GET Input NOT captured  
-3. has one get param 'q'
+2. GET Input NOT captured  Libraries: {requests and urllib failed to load data or access website}
+3. It has one GET Param = ['q']
 
 ISSUE NO. 7:    [FIXED and Captured the Values]
 https://atasteofkentucky.com/kentucky-derby-2020/
@@ -109,9 +109,12 @@ Issue 10: [I just wanted to Make sure that for every GET Param [name,value] pair
 2. Tool    => https://www.timberland.co.uk/shop/SearchDisplay?sType=SimpleSearch&catalogId=11159&searchSource=Q&beginIndex=0&storeId=7105&langId=-11&searchTerm=(uvw"xyz'yxz</zxy
 [ So Tool-Created URL was SAME as Manual Finding fo the URL]
 
-ISSUE 11:
-1. If 2 forms have same 2 GET Params, then unique GET URLS are extracted and GET URLs are formed. [2 unique urls in this case]
-2.      What if FORMS have differect FORM-ACTIONS, then there should be 4 GET URLS.
+ISSUE 11:   [ Resolved ]
+Unique GET URLs:
+    a. If [2] forms have same [2] GET Params, then [4] GET URLs will be generated and then [2] unique GET URLS are extracted
+    b. BUT if they have DIFFERENT FORM ACTIONS, then there will be 4 UNIQUE GET URLS.
+
+
 
 """
 
